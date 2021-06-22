1b7638b1ca81829d2a43114402f481e1ee3c185e
Peter Hansen <peterhansen1198@gmail.com>
2021-06-22 14:56:18 -0500

    Fixed crash from not using switchcompat

--- app/src/main/java/com/sticknology/jani/run2/Run2.java
+++ app/src/main/java/com/sticknology/jani/run2/Run2.java
@@ -1,10 +1,29 @@
 package com.sticknology.jani.run2;
 
+import com.sticknology.jani.data.Workout;
+import com.sticknology.jani.data2.MyTime;
+
+import java.util.ArrayList;
+
 public class Run2 {
 
+    private String mTitle, mNotes, mType;
+    private MyTime mTimeOfDay;
+    private ArrayList<Interval2> mIntervals;
+    private ArrayList<Workout> mWorkouts;
+
     //Object creation method
-    public Run2(){
+    public Run2(String title, String notes, String type, MyTime timeOfDay,
+                ArrayList<Interval2> intervals, ArrayList<Workout> workouts){
 
-
+        mTitle = title;
+        mNotes = notes;
+        mType = type;
+        mTimeOfDay = timeOfDay;
+        mIntervals = intervals;
+        mWorkouts = workouts;
     }
+
+    //Only getters and setters below here
+
 }
